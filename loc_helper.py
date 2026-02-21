73b7bf0345da43549c8f0594ea272ee230437ed5
Anthony Jiang <anthony.zyjiang@outlook.com>
2026-02-21 13:02:51 +0000

    added break multiplier

--- loc_helper.py
+++ loc_helper.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import json
+import re
 import sys
 
 import rich
@@ -47,8 +48,11 @@ def replace_value(work_file, old_value, new_value, auto_approve=False):
         console.print(f"[{i+1}/{len(to_be_changed)}]")
         loc_new = value["loc_new"].replace(old_value, new_value)
 
-        comparison_text = f"{'-'*10}\n[bold]Before:[/bold]\n[red]{highlighter.highlight(value['loc_new']
-            )}[/red] \n[bold]After:[/bold]\n[green]{highlighter.highlight(loc_new)}[/green]\n\n"
+        comparison_text = "-" * 10 + "\n"
+        comparison_text += "[bold]Before:[/bold]\n"
+        comparison_text += f"[red]{highlighter.highlight(value['loc_new'])}[/red] \n"
+        comparison_text += "[bold]After:[/bold]\n"
+        comparison_text += f"[green]{highlighter.highlight(loc_new)}[/green]\n\n"
         comparisons.append(comparison_text)
 
         if auto_approve:
@@ -78,6 +82,36 @@ def replace_value(work_file, old_value, new_value, auto_approve=False):
     save_work_file(work_file)
 
 
+def add_break_multiplier():
+    work_file = load_work_file()
+    for key, value in work_file.items():
+        if key.split(".")[0] == "ST_Tutorial_Cards":
+            continue
+        # look for <text color=\"#808080\">[</><text color=\"#D5B87C\">1x</><text color=\"#808080\">]</>
+        # and add it before the first line change or first "。"
+        match = re.search(
+            r'<text color=\"#808080\">\[</><text color=\"#D5B87C\">.*</><text color=\"#808080\">\]</>',
+            value["enhanced"]
+            )
+        if match:
+            # find the first \n or \r\n or first "。"
+            line_change = re.search(r'\n|\r\n', value["loc_new"])
+            if line_change:
+                value["loc_new"] = value["loc_new"][:line_change.start()] \
+                                    + match.group(0) \
+                                    + value["loc_new"][line_change.start():]
+            else:
+                # find the first "。"
+                period = re.search(r'。', value["loc_new"])
+                if period:
+                    value["loc_new"] = value["loc_new"][:period.start()] \
+                                        + match.group(0) \
+                                        + value["loc_new"][period.start():]
+                else:
+                    value["loc_new"] = value["loc_new"] + match.group(0)
+    save_work_file(work_file)
+
+
 def main(args: list[str]):
     if len(args) == 0:
         raise ValueError("Usage: python loc_helper.py -r <old_value> <new_value> [-y]")
@@ -90,6 +124,8 @@ def main(args: list[str]):
         print(f"Replacing {old_value} with {new_value}")
         work_file = load_work_file()
         replace_value(work_file, old_value, new_value, auto_approve)
+    elif args[0] == "-b":
+        add_break_multiplier()
     else:
         raise ValueError("Usage: python loc_helper.py -r <old_value> <new_value> [-y]")
 
